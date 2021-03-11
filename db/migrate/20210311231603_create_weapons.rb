5fc5985591fa50c7893d65f7e9b9c4b01d952bcf
Christopher Breen <C.jbreen61@gmail.com>
2021-03-11 18:17:35 -0500

    used scaffold to build mvc for weapons

---
+++ db/migrate/20210311231603_create_weapons.rb
@@ -0,0 +1,20 @@
+class CreateWeapons < ActiveRecord::Migration[6.1]
+  def change
+    create_table :weapons do |t|
+      t.string :name
+      t.string :weapon_type
+      t.string :muzzle
+      t.string :barrel
+      t.string :laser
+      t.string :optic
+      t.string :stock
+      t.string :underbarrel
+      t.string :ammunition
+      t.string :rear_grip
+      t.string :perk
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
