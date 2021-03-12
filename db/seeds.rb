5802df54467dacd1fece36584b0bb2f6d938c2fc
Christopher Breen <C.jbreen61@gmail.com>
2021-03-12 11:12:31 -0500

    reseeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,4 @@ warzone = Game.create(name:"WarZone", publisher:"Activision",)
 black_ops = Game.create(name:"Black-Ops Cold War", publisher:"Activision",)
 
 Weapon.create(name:"Kilo 141", weapon_type:"AR",laser:"Tac Laser", stock:"No Stock", underbarrel:"Commando Foregrip", rear_grip:"Stippled Grip Tape", perk:"Sleight of Hand",game_id: warzone.id)
+Weapon.create(name:"MAC 10", weapon_type:"SMG",laser:"Tac Laser", stock:"No Stock", underbarrel:"Commando Foregrip", rear_grip:"Stippled Grip Tape", perk:"Sleight of Hand",game_id: black_ops.id)
